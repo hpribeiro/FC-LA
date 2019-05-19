83ed75f34abac90e93fc0bcb1d179b6bad6c88b6
Henrique Ribeiro <hepr94@gmail.com>
2019-05-19 11:39:24 -0300

    feat(app.component.html): add footer component

--- lookAfter/src/app/app.module.ts
+++ lookAfter/src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { NavbarComponent } from './navbar/navbar.component'
+import { FooterComponent } from './footer/footer.component'
+import { LoginComponent } from './login/login.component'
+import { UserFormComponent } from './user-form/user-form.component'
+import { ModalModule } from 'ngx-bootstrap/modal'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     DashboardComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
     NavbarComponent,
+    UserFormComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    ModalModule.forRoot(),
+  ],
+  entryComponents: [LoginComponent, UserFormComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
