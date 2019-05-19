02e17fdb0e2081401ad989524ac35335372e6738
Henrique Ribeiro <hepr94@gmail.com>
2019-05-19 10:52:38 -0300

    feat(navbar.ts): create methods to open modals and scroll

--- lookAfter/src/app/navbar/navbar.component.ts
+++ lookAfter/src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core'
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal'
+import { LoginComponent } from '../login/login.component'
+import { UserFormComponent } from '../user-form/user-form.component'
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +10,39 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  bsModalRef: BsModalRef
+  config = {
+    animated: true,
+  }
+  isLogged = false
+  constructor(private modalService: BsModalService, private router: Router) {
+    router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.urlAfterRedirects
+        this.isLogged = url === '/home' ? false : true
+        console.log(url, this.isLogged)
+      }
+    })
+  }
 
   ngOnInit() {}
+
+  openLoginModal() {
+    this.bsModalRef = this.modalService.show(LoginComponent, this.config)
+  }
+
+  openUserFormModal() {
+    this.bsModalRef = this.modalService.show(UserFormComponent, this.config)
+  }
+
+  scroll(id: string) {
+    console.log(document.getElementById('contact'))
+    document
+      .getElementById(id)
+      .scrollIntoView({ block: 'end', behavior: 'smooth' })
+  }
+
+  logout() {
+    this.router.navigateByUrl('/home')
+  }
 }
